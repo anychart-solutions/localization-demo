aef880bb11bef399ac4f0772f51454ea3909bd02
FilippovAM <andrey.filippov@anychart.com>
2016-09-06 12:52:13 +0800

    fix

--- build/js/common.js
+++ build/js/common.js
@@ -3,6 +3,7 @@
     var text_doc = document.documentElement.innerHTML;
     var array_url = [];
     var chart_container = 'acme-chart';
+    var $date_pattern = $('.date-pattern');
     formats = {};
 
     function hidePreloader() {
@@ -44,7 +45,7 @@
     }
 
     function changePattern(code) {
-        var $table = $('.date-pattern').find('tbody');
+        var $table = $date_pattern.find('tbody');
         $table.empty();
 
         for (var i = 0; i < formats[code].length; i++) {
@@ -150,7 +151,7 @@
 
         function reDrawChart() {
             if (window['anychart']['format']['locales'][code] != undefined) {
-                var format = $('.date-pattern').find('td.active').text();
+                var format = $date_pattern.find('td.active').text();
 
                 clearInterval(timerId);
                 disposeChart();
@@ -180,7 +181,7 @@
     }
 
     function askEventDatePattern() {
-        $('.date-pattern').find('td').on('click', function () {
+        $date_pattern.find('td').on('click', function () {
             var $that = $(this);
 
             activeEl($that);
