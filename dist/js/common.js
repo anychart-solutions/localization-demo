158dfedd7350f6aa3bd3b8243a2020a7843b2170
FilippovAM <andrey.filippov@anychart.com>
2016-09-26 11:54:30 +0800

    fix

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,11 +3,13 @@
     var product = $('body').data('product');
     var chart_container = 'acme-chart';
     var $date_pattern = $('.date-pattern');
-    var default_format = 'MMM';
+    var default_format = 'EEEE, dd MMMM yyyy';
     formats = {};
 
     function hidePreloader() {
-        $('#loader-wrapper').fadeOut('slow');
+        $('#loader-wrapper').fadeOut('slow', function () {
+            scrollPosition();
+        });
     }
 
     function activeEl(el) {
@@ -206,10 +208,7 @@
     }
 
     function createChart(container, locale, format) {
-        // Set a localization for output.
-        anychart.format.outputLocale(locale);
-        // create data set on our data,also we can pud data directly to series
-        var dataSet = anychart.data.set([
+        var data = [
             ['Jan 2015', 22, 43, 75],
             ['Feb 2015', 34, 45, 56],
             ['Mar 2015', 16, 26, 67],
@@ -222,7 +221,16 @@
             ['Oct 2015', 18, 31, 58],
             ['Nov 2015', 24, 42, 69],
             ['Dec 2015', 29, 39, 71]
-        ]);
+        ];
+
+        var _title = 'ACME corp. Problems During the Year\n' + 'From: ' +
+            anychart.format.dateTime(data[0][0], format, -8 * 60, locale) +
+            '\nTo: ' + anychart.format.dateTime(data[data.length - 1][0], format, -8 * 60, locale);
+
+        // Set a localization for output.
+        anychart.format.outputLocale(locale);
+        // create data set on our data,also we can pud data directly to series
+        var dataSet = anychart.data.set(data);
 
         // map data for the first series,take value from first column of data set
         var seriesData_1 = dataSet.mapAs({x: [0], value: [1]});
@@ -244,20 +252,9 @@
         // set container id for the chart
         chart.container('container');
         // set chart title text settings
-        chart.title('ACME corp. Problems During the Year');
-        chart.title().padding([20, 0, 10, 0]);
+        chart.title(_title).padding([20, 0, 10, 0]);
         // set yAxis title
         chart.yAxis().title('Occurences per month');
-
-        /** We can edit series stroke by function in which context available
-         *  @param color - stroke color
-         *  @param style - dash lines style
-         *  @return string/Object - acgraph.vector.Stroke type (string/Object)
-         */
-        var seriesStrokeFunction = function (color, style) {
-            return {color: color, dash: style};
-        };
-
 
         /** Helper Function to setup series
          *  @param series - stroke color
@@ -282,33 +279,29 @@
 
         // setup second series
         series = chart.line(seriesData_2);
-        series.stroke(seriesStrokeFunction('#3C8AD8', '3 5 10 5'));
+        series.stroke('#3C8AD8');
         seriesConfiguration(series, 'Delivery Failure');
 
         // setup third series
         series = chart.line(seriesData_3);
-        series.stroke(seriesStrokeFunction('#F18126', '3 5'));
+        series.stroke('#F18126');
         seriesConfiguration(series, 'Order Cancellation');
-
-        // interactivity and tooltip settings
-        chart.interactivity().hoverMode('byX');
-        chart.tooltip().displayMode('separated');
-        chart.tooltip().positionMode('point');
 
         // turn the legend on
         chart.legend().enabled(true).padding([0, 0, 10, 0]);
 
         chart.xAxis().labels().textFormatter(function () {
-            return anychart.format.dateTime(this.value, format, -8 * 60, locale);
+            return anychart.format.dateTime(this.value, 'MMM', -8 * 60, locale);
         });
         chart.tooltip().titleFormatter(function () {
             return anychart.format.dateTime(this.points[0].x, format, -8 * 60, locale);
         });
 
-        // set chart tooltip and interactivity settings
-        chart.tooltip().position('top').anchor('centerbottom');
-        chart.tooltip().positionMode('point');
-        chart.interactivity().hoverMode('byX');
+        chart.tooltip().textFormatter(function () {
+            return this.points[0].seriesName + ': ' + this.points[0].value +
+                '\n' + this.points[1].seriesName + ': ' + this.points[1].value +
+                '\n' + this.points[2].seriesName + ': ' + this.points[2].value;
+        });
         // set container id for the chart
         chart.container(container);
         // initiate chart drawing
@@ -338,6 +331,16 @@
         placeBlocks();
     });
 
+    function scrollPosition() {
+        var $language_locale = $('.language-locale');
+        var top = $language_locale.find('.active').offset().top -
+            $language_locale.height() / 2 + $language_locale.find('.active').height() / 2;
+
+        $language_locale.animate({
+            scrollTop: top
+        }, 500);
+    }
+
     function placeBlocks() {
         var mq = window.matchMedia('(max-width: 768px)');
 
@@ -351,14 +354,16 @@
     /* Prism copy to clipbaord */
     $('pre.copytoclipboard').each(function () {
         $this = $(this);
-        $button = $('<button style="font-size: 12px;">Copy</button>');
+        $button = $('<button></button>');
         $this.wrap('<div/>').removeClass('copytoclipboard');
         $wrapper = $this.parent();
         $wrapper.addClass('copytoclipboard-wrapper').css({position: 'relative'});
         $button.css({
             position: 'absolute',
             top: 10,
-            right: 27
+            right: 27,
+            width: 55,
+            height: 31
         }).appendTo($wrapper).addClass('copytoclipboard btn btn-default');
 
         var copyCode = new Clipboard('button.copytoclipboard', {
@@ -368,9 +373,9 @@
         });
         copyCode.on('success', function (event) {
             event.clearSelection();
-            event.trigger.textContent = 'Copied';
+            $(event.trigger).addClass('copied');
             window.setTimeout(function () {
-                event.trigger.textContent = 'Copy';
+                $(event.trigger).removeClass('copied');
             }, 2000);
         });
         copyCode.on('error', function (event) {
