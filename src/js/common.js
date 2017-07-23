48adbaf7590024da98ef56e10797cd4d9257e490
FilippovAM <cyberross@rambler.ru>
2017-07-23 14:48:25 +0800

    fix

--- src/js/common.js
+++ src/js/common.js
@@ -121,12 +121,15 @@
             .replace('function createChart(container, locale, format) {', '')
             .replace(/}$/, '')
             .trim();
-        var code = 'anychart.onDocumentReady(function () {\n\t\tvar format ="' + format + '";\n' +
-            '\t\tvar locale = "' + locale + '";\n\t\t' + code_func + '\n\t\t});';
+        var code = 'anychart.onDocumentReady(function () {' +
+            '\n\t\tvar format ="' + format + '";\n' +
+            '\t\tvar locale = "' + locale + '";\n' +
+            '\t\tvar timeZoneOffset = new Date().getTimezoneOffset();\n\t\t' +
+            code_func + '\n\t\t});';
         var doc = '<!DOCTYPE html>\n<html lang="en">\n<head>' +
             '\n\t<meta charset="utf-8" />' +
             '\n\t<script src="http://cdn.anychart.com/geodata/1.2.0/custom/world/world.js"></script>' +
-            '\n\t<script src="http://anychart.com/products/anygantt/demos/localization/repo/anychart-bundle.min.js"></script>' +
+            '\n\t<script src="https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js"></script>' +
             '\n\t<script src="' + 'https://cdn.anychart.com/locale/1.1.0/' + locale + '.js"></script>' +
             '\n</head>\n<body>' +
             '\n\t<div id="container" style="width: 850px; height: 600px; margin: 0 auto;"></div>' +
