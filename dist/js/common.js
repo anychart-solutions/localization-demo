f1d8a43b1f7946d79fca696c0e580ecc4ebf48bd
FilippovAM <andrey.filippov@anychart.com>
2017-10-10 13:50:27 +0800

    fix

--- dist/js/common.js
+++ dist/js/common.js
@@ -127,6 +127,7 @@
             code_func + '\n\t\t});';
         var doc = '<!DOCTYPE html>\n<html lang="en">\n<head>' +
             '\n\t<meta charset="utf-8" />' +
+            '\n\t<link rel="stylesheet" href="https://cdn.anychart.com/releases/8.0.0/css/anychart-ui.min.css">' +
             '\n\t<script src="https://cdn.anychart.com/releases/8.0.0/js/anychart-base.min.js"></script>' +
             '\n\t<script src="https://cdn.anychart.com/releases/8.0.0/js/anychart-exports.min.js"></script>' +
             '\n\t<script src="https://cdn.anychart.com/releases/8.0.0/js/anychart-ui.min.js"></script>' +
